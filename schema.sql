aa9e29a5c4d418a963eb1819f56fb53c448d7180
Alex <av.ermakovich@gmail.com>
2019-04-26 15:09:32 +0300

    9. Создание схемы БД личного проекта

---
+++ schema.sql
@@ -0,0 +1,40 @@
+CREATE TABLE project (
+id INT AUTO_INCREMENT PRIMARY KEY,
+name INT
+);
+CREATE TABLE user (
+id INT AUTO_INCREMENT PRIMARY KEY,
+day DATETIME,
+name INT,
+password INT,
+avatar INT,
+contact INT,
+lot_id INT,
+bet_id INT
+);
+CREATE TABLE category (
+id INT AUTO_INCREMENT PRIMARY KEY,
+name INT,
+code INT,
+UNIQUE KEY (name, code)
+);
+CREATE TABLE lot (
+id INT AUTO_INCREMENT PRIMARY KEY,
+day DATETIME,
+name INT,
+description INT,
+image INT,
+start_price INT,
+date_finish INT,
+step_lot INT,
+autor_id INT,
+win_user_id INT,
+category_id INT
+);
+CREATE TABLE bet (
+id INT AUTO_INCREMENT PRIMARY KEY,
+day DATETIME,
+price INT,
+user_id INT,
+lot_id INT
+);
