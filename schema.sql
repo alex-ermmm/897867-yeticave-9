1d9dc10c700b6c04e4a93a623342ea32ac7fdbe2
Alex <av.ermakovich@gmail.com>
2019-04-28 22:42:30 +0300

    Уникальные коды и email

--- schema.sql
+++ schema.sql
@@ -3,18 +3,21 @@ USE alexandr_yermakovich_yeticave;
 CREATE TABLE user (
 user_id INT AUTO_INCREMENT PRIMARY KEY,
 regestration_date DATETIME,
+email CHAR,
 name CHAR,
 password CHAR,
 avatar CHAR,
 contact TEXT,
-lot_id INT,
-bet_id INT
+CREATE INDEX index_email ON user (email),
+CREATE INDEX index_name ON user (name),
+UNIQUE KEY (email)
 );
 CREATE TABLE category (
 category_id INT AUTO_INCREMENT PRIMARY KEY,
 name CHAR,
 code CHAR,
-UNIQUE KEY (name, code)
+UNIQUE KEY (name, code),
+CREATE INDEX index_category ON category (name)
 );
 CREATE TABLE lot (
 lot_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -30,7 +33,9 @@ win_user_id INT,
 category_id INT,
 FOREIGN KEY (autor_id)  REFERENCES user (user_id),
 FOREIGN KEY (win_user_id)  REFERENCES user (user_id),
-FOREIGN KEY (category_id)  REFERENCES category (category_id)
+FOREIGN KEY (category_id)  REFERENCES category (category_id),
+CREATE INDEX index_name ON lot (name),
+CREATE INDEX index_description ON lot (description)
 );
 CREATE TABLE bet (
 id INT AUTO_INCREMENT PRIMARY KEY,
@@ -39,5 +44,6 @@ price INT,
 user_id INT,
 lot_id INT,
 FOREIGN KEY (user_id)  REFERENCES user (user_id),
-FOREIGN KEY (lot_id)  REFERENCES lot (lot_id)
+FOREIGN KEY (lot_id)  REFERENCES lot (lot_id),
+CREATE INDEX index_price ON bet (price)
 );
