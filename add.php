8630aed5bf01b0ec52fde5955c7f70e95b0012e9
Alex <av.ermakovich@gmail.com>
2019-05-19 21:30:50 +0300

    fix

--- add.php
+++ add.php
@@ -8,26 +8,93 @@ if ($link == false){
 }
 else {
     $sql_lot = 'SELECT cat.name AS cat_name, l.name, l.lot_id, l.image, l.start_price, l.step_lot, l.autor_id, l.date_finish, (SELECT COUNT(bet.id) FROM bet WHERE bet.lot_id = l.lot_id) lot_count FROM lot l RIGHT JOIN category cat ON cat.category_id = l.category_id WHERE l.date_finish > NOW() ORDER BY l.lot_id DESC';
-    $sql_cat = 'SELECT * FROM category';       
-      
-	if (($res_lot = mysqli_query($link, $sql_lot)) and ($res_cat = mysqli_query($link, $sql_cat))) 
+    $sql_cat = 'SELECT * FROM category';  
+
+    if (($res_lot = mysqli_query($link, $sql_lot)) and ($res_cat = mysqli_query($link, $sql_cat))) 
     {
         $lots = mysqli_fetch_all($res_lot, MYSQLI_ASSOC);
-        $category = mysqli_fetch_all($res_cat, MYSQLI_ASSOC);	
-        $page_content = include_template('lot_add_tpl.php', ['lots' => $lots, 'category' => $category]);  
-       // print_r($lots);
+        $category = mysqli_fetch_all($res_cat, MYSQLI_ASSOC);   
     }
     else {
         print mysqli_error($link);
     }
 
-$layout_content = include_template('layout_pages.php', [
-    'page_content' => $page_content,
-    'category' => $category,
-    'title' => 'YetiCave',
-    'is_auth' => $is_auth,
-    'user_name' => $user_name
-]);
+       if (($_SERVER['REQUEST_METHOD'] == 'POST') and (array_key_exists("lot", $_POST))) {
 
-print($layout_content);
-}
+            $add_lot = $_POST;
+            $add_lot_image = $_FILES;
+
+
+            $required = ['title', 'category', 'description',  'start_price', 'step-lot', 'date-finish'];
+            $dict = ['title' => 'Название', 'category' => 'Категория', 'description' => 'Описание', 'start_price' => 'Начальная цена', 'step-lot' => 'Шаг ставки', 'date-finish' => 'Дата окончания торгов', 'image_type' => 'Изображение', 'no_file' => 'Изображение'];
+            $errors = [];
+
+            foreach ($required as $key) {
+                if (empty($_POST['lot'][$key])){
+                   $error[$key] = ' Это поле надо заполнить';
+                }
+            }
+
+            if (empty($_FILES['image']['name'])) 
+            {
+                $error['no_file'] = 'Вы не загрузили файл';                
+            }
+            else 
+            {
+                $tmp_name = $_FILES['image']['tmp_name'];
+                $path = $_FILES['image']['name'];
+
+                $finfo = finfo_open(FILEINFO_MIME_TYPE);
+                $file_type = finfo_file($finfo, $tmp_name);
+                
+                if (($file_type !== "image/png") or ($file_type !== "image/jpg") or ($file_type !== "image/jpeg"))
+                {
+                    $error['image_type'] = 'Загрузите картинку в формате нужном формате: jpg, jpeg, png';
+                }
+                else {
+                    move_uploaded_file($tmp_name, 'uploads/' . $path);
+                    $add_lot['path'] = $path;
+                } 
+            }
+
+            if (count($error)) 
+            {
+                $page_content = include_template('lot_add_tpl.php', ['add_lot' => $add_lot, 'error' => $error, 'dict' => $dict, 'lots' => $lots, 'category' => $category]);
+                $layout_content = include_template('layout_pages.php', [
+                'page_content' => $page_content,
+                'category' => $category,
+                'title' => 'YetiCave. Ошибки в форме.',
+                'is_auth' => $is_auth,
+                'user_name' => $user_name
+                ]);
+                print($layout_content);
+            }
+            
+            else 
+            {
+                $page_content = "все ок. тут будет форма показа загруженного лота";
+                $layout_content = include_template('layout_pages.php', [
+                'page_content' => $page_content,
+                'category' => $category,
+                'title' => 'YetiCave лот.',
+                'is_auth' => $is_auth,
+                'user_name' => $user_name
+                ]);
+                print($layout_content);
+            }
+
+        }
+        else {
+            print "REQUEST_METHOD = POST. НЕТ";
+            $page_content = include_template('lot_add_tpl.php', ['lots' => $lots, 'category' => $category]); 
+            $layout_content = include_template('layout_pages.php', [
+            'page_content' => $page_content,
+            'category' => $category,
+            'title' => 'YetiCave. Форма добавления лота',
+            'is_auth' => $is_auth,
+            'user_name' => $user_name
+        ]);
+
+        print($layout_content);
+        }
+    }
--- templates/lot_add_tpl.php
+++ templates/lot_add_tpl.php
@@ -1,60 +1,66 @@
-<?php
-if(isset($_POST)){
-  print_r($_POST);
-}
-else
- print $_POST."пуст";
-?>
 <form class="form form--add-lot container form--invalid" action="add.php" method="post" enctype="multipart/form-data"> <!-- form--invalid -->
       <h2>Добавление лота</h2>
       <div class="form__container-two">
-        <div class="form__item form__item--invalid"> <!-- form__item--invalid -->
+        <div class="form__item <? if(isset($error['title'])) : print "form__item--invalid"; endif;?>"> 
           <label for="lot-name">Наименование <sup>*</sup></label>
-          <input type="text" name="lot[title]" id="name" value="" placeholder="Введите наименование лота">
-          <span class="form__error">Введите наименование лота</span>
+          <input type="text" name="lot[title]" id="name" value="<?=$_POST['lot']['title']?>" placeholder="Введите наименование лота">
+          <? if(isset($error['title'])) : print "<span class='form__error'> ".$error['title']." </span>"; endif;?>
         </div>
-        <div class="form__item">
+        <div class="form__item <? if($_POST['lot']['category'] == "Выберите категорию") : print "form__item--invalid"; endif;?>">
           <label for="category">Категория <sup>*</sup></label>
           <select id="category" name="lot[category]">
-            <option>Выберите категорию</option>
+            <option >Выберите категорию</option>
             <?php foreach ($category as $categories) { ?>      
-                <option value="<?=$categories['category_id']?>"><?=$categories['name']?></option>
+                <option value="<?=$categories['category_id']?>" 
+                  <?if((isset($_POST['lot']['category'])) and ($_POST['lot']['category']) == $categories['category_id'] ): echo "selected"; endif;?> ><?=$categories['name']?></option>
             <?php } ?>
           </select>
-          <span class="form__error">Выберите категорию</span>
+          <? if($_POST['lot']['category'] == "Выберите категорию") : print "<span class='form__error'>Выберите категорию</span>"; endif;?>
         </div>
       </div>
-      <div class="form__item form__item--wide">
+      <div class="form__item form__item--wide <? if(isset($error['description'])) : print "form__item--invalid"; endif;?>">
         <label for="message">Описание <sup>*</sup></label>
-        <textarea name="lot[description]" id="description" placeholder="Напишите описание лота"></textarea>
-        <span class="form__error">Напишите описание лота</span>
+        <textarea name="lot[description]" id="description" placeholder="Напишите описание лота"><?=$_POST['lot']['description']?></textarea>
+        <? if(isset($error['description'])) : print "<span class='form__error'>Напишите описание лота</span>"; endif;?>
       </div>
       <div class="form__item form__item--file">
         <label>Изображение <sup>*</sup></label>
         <div class="form__input-file">
-          <input class="visually-hidden" type="file" name="lot[image]" id="image" value="">
+          <a href="<?=$_FILES['tmp_name']['image']?>"><?=$_FILES['lot']['image']['name']?></a>
+          <input class="visually-hidden" type="file" name="image" id="image" value="<?=$_FILES['lot']['image']?>">
           <label for="image">
             Добавить
           </label>
+          <? if(isset($error['image'])) : print "<span class='form__error'>Добавте изображение</span>"; endif;?>
         </div>
       </div>
       <div class="form__container-three">
-        <div class="form__item form__item--small">
+        <div class="form__item form__item--small <? if(isset($error['start_price'])) : print "form__item--invalid"; endif;?>">
           <label for="lot-rate">Начальная цена <sup>*</sup></label>
-          <input type="text" name="lot[start_price]" id="lot-rate" value="" placeholder="0">
-          <span class="form__error">Введите начальную цену</span>
+          <input type="text" name="lot[start_price]" id="lot-rate" value="<?=$_POST['lot']['start_price']?>" placeholder="0">          
+          <? if(isset($error['start_price'])) : print "<span class='form__error'>Введите начальную цену</span>"; endif;?>
         </div>
-        <div class="form__item form__item--small">
+        <div class="form__item form__item--small <? if(isset($error['step-lot'])) : print "form__item--invalid"; endif;?>">
           <label for="lot-step">Шаг ставки <sup>*</sup></label>
-          <input type="text" name="lot[step-lot]" id="step-lot" value="" placeholder="0">
-          <span class="form__error">Введите шаг ставки</span>
+          <input type="text" name="lot[step-lot]" id="step-lot" value="<?=$_POST['lot']['step-lot']?>" placeholder="0">
+          <? if(isset($error['step-lot'])) : print "<span class='form__error'>Введите шаг ставки</span>"; endif;?>
         </div>
-        <div class="form__item">
+        <div class="form__item <? if(isset($error['date-finish'])) : print "form__item--invalid"; endif;?>">
           <label for="lot-date">Дата окончания торгов <sup>*</sup></label>
-          <input class="form__input-date" id="lot-date" type="text" name="lot[date-finish]"  value=""  placeholder="Введите дату в формате ГГГГ-ММ-ДД">
-          <span class="form__error">Введите дату завершения торгов</span>
+          <input class="form__input-date" id="lot-date" type="text" name="lot[date-finish]"  value="<?=$_POST['lot']['date-finish']?>"  placeholder="Введите дату в формате ГГГГ-ММ-ДД">          
+          <? if(isset($error['date-finish'])) : print "<span class='form__error'>Введите дату завершения торгов</span>"; endif;?>
         </div>
+      </div>     
+      <?php 
+      if (isset($error)): ?>
+      <div class="form__error">
+        <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
+        <ul>
+          <?php foreach($error as $err => $val): ?>
+          <li><strong><?=$dict[$err];?>:</strong> <?=$val;?></li>
+          <?php endforeach; ?>
+        </ul>
       </div>
-      <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
+    <?php endif; ?>
       <button type="submit" class="button">Добавить лот</button>
     </form>
